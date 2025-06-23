b6456fc2e0aa7d27a2ce4da1a4091e10f03115fa
vrushabh <vrushabhkumatale1999@gmail.com>
2025-06-23 12:43:23 +0530

    Assign address To User Functionality Completed

--- spring_boot_backend_template/src/main/java/com/sunbeam/controller/UserController.java
+++ spring_boot_backend_template/src/main/java/com/sunbeam/controller/UserController.java
@@ -2,11 +2,13 @@ package com.sunbeam.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sunbeam.dto.AddressDTO;
 import com.sunbeam.dto.AuthRequest;
 import com.sunbeam.dto.UserRequestDTO;
 import com.sunbeam.service.UserService;
@@ -35,5 +37,13 @@ public class UserController {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(userService.signUp(dto));
 		
+	}
+	
+	@PostMapping("/{userId}/address")
+	public ResponseEntity<?> assignUserAddress(
+			@PathVariable Long userId,@RequestBody AddressDTO dto) {
+		System.out.println("assign adr ");
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(userService.assignAddress(userId,dto));
 	}
 }
