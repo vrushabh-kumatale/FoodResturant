487778c59d9634508f5694e4c000d429ca38771d
vrushabh <vrushabhkumatale1999@gmail.com>
2025-07-22 08:24:25 +0530

    Cors added In User Controller and Front End App Created

--- spring_boot_backend_template/src/main/java/com/sunbeam/controller/UserController.java
+++ spring_boot_backend_template/src/main/java/com/sunbeam/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sunbeam.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
 	private final UserService userService;
